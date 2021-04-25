a8dfb77a370ccfd9cb6f0cbb9c887dfd385ed70b
JoaoTS20 <joaots@ua.pt>
2021-04-25 12:46:31 +0100

    Sign Up Working

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,30 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Aqui depois mudar option alguns
+from django.contrib.auth.models import User
+
+from app.models import NormalUser
+
+
 class TeamFilterForm(forms.Form):
     full_name = forms.CharField(label='Search by Name:', required=False)
     country = forms.CharField(label='Search by Country:', required=False)
     competition = forms.CharField(label='Search by Competition:', required=False)
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="Email Address")
+    job_football_related = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for myField in self.fields:
+            if myField != 'job_football_related':
+                self.fields[myField].widget.attrs['class'] = 'form-control'
+                self.fields[myField].widget.attrs['placeholder'] = self.fields[myField].label
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2", "job_football_related",)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 import datetime
 
 # Create your views here.
-from app.forms import TeamFilterForm
-from app.models import Staff, Team, Competition, ClubPlaysIn
+from app.forms import TeamFilterForm, SignUpForm
+from app.models import Staff, Team, Competition, ClubPlaysIn, NormalUser
 
 
 def test(request):
@@ -27,7 +28,8 @@ def teams(request):
             competition = form.cleaned_data['competition']
 
             t_parms = {
-                'teams': Team.objects.filter(full_name__contains=full_name, country__contains=country, clubplaysin__competition__full_name__contains=competition),
+                'teams': Team.objects.filter(full_name__contains=full_name, country__contains=country,
+                                             clubplaysin__competition__full_name__contains=competition),
                 'form': TeamFilterForm()
             }
             return render(request, 'teams.html', t_parms)
@@ -51,7 +53,20 @@ def players(request):
 
 
 def signup(request):
-    return render(request, 'signup.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            job_football_related = form.cleaned_data.get('job_football_related')
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            NormalUser(user=user,job_football_related=job_football_related).save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def profile(request):
